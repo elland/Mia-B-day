d35176de6209cdfee3005872f29a94dfede85cb7
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-06-30 20:23:39 -0300

    this is so cool

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,6 +1,10 @@
 class DisplayController < ApplicationController
   def index
 		Message.create!(:content => "Mia, we love you!")
-		@msgs = Message.all
+		@msgs = Message.all.in_groups_of(16)
+		@msgs.each do |msg|
+			msg.compact!
+		end
+		@count = Message.count
 	end
 end
