5101acfc722ffe53882f467071df4de6b5d022ab
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-06-30 20:10:38 -0300

    new architecture

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,18 +1,6 @@
 class DisplayController < ApplicationController
   def index
-		if Message.all.empty?
-			@msg = Message.create(:content => "")
-		else
-			@msg = Message.first
-		end
-		begin
-			@msg.content += "Mia, we love you!<br/>"
-			@count = @msg.content.split('<br/>').count
-			@msg.save
-			@msg = @msg.content.split('<br/>').in_groups_of(16)
-			@msg.compact!
-		rescue
-			flash[:alert] = "error"
-		end
+		Message.create!(:content => "Mia, we love you!")
+		@msgs = Message.all
 	end
 end
